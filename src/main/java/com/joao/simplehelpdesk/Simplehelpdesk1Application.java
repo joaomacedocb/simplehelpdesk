084630e23f97452a2cc865a8a2035d1ad157017a
joaomacedocb <joaomacedocb@gmail.com>
2024-07-21 12:35:48 -0300

    Initial commit.

---
+++ src/main/java/com/joao/simplehelpdesk/Simplehelpdesk1Application.java
@@ -0,0 +1,13 @@
+package com.joao.simplehelpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Simplehelpdesk1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Simplehelpdesk1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/joao/simplehelpdesk/Simplehelpdesk1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joao.simplehelpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Simplehelpdesk1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
