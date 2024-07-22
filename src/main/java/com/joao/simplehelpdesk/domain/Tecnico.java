cd8d21f61d15bcac9f927105daad79c7e18745fd
joaomacedocb <joaomacedocb@gmail.com>
2024-07-22 00:10:49 -0300

    Adicionada as anotacoes e realizado relacionamento entre classes chamados, clientes e tecnicos

--- src/main/java/com/joao/simplehelpdesk/domain/Tecnico.java
+++ src/main/java/com/joao/simplehelpdesk/domain/Tecnico.java
@@ -3,18 +3,27 @@ package com.joao.simplehelpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.joao.simplehelpdesk.domain.enums.Perfil;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Tecnico extends Pessoa {
 	
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Tecnico() {
 		super();
-		// TODO Auto-generated constructor stub
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
-		// TODO Auto-generated constructor stub
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {
