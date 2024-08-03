6397d6367f95a322aebd8eb2b71d7ef07e3f3a02
joaomacedocb <joaomacedocb@gmail.com>
2024-08-03 09:44:48 -0300

    Implementado metodo para buscar todos os chamados via API

--- src/main/java/com/joao/simplehelpdesk/services/ChamadoService.java
+++ src/main/java/com/joao/simplehelpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.joao.simplehelpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,10 @@ public class ChamadoService {
 	public Chamado findById (Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Chamado não encontrado na base de dados. ID: " + id ));
+	}
+
+	public List<Chamado> findAll() {
+		return repository.findAll();
 	}
 	
 	
