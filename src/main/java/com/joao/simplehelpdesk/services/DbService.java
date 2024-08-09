1bf4c1215e7a2ddf8a4ac14ec746e991dbfab1ab
joaomacedocb <joaomacedocb@gmail.com>
2024-08-09 15:04:19 -0300

    Ajustes referente a spring security e seguranca de dados

--- src/main/java/com/joao/simplehelpdesk/services/DbService.java
+++ src/main/java/com/joao/simplehelpdesk/services/DbService.java
@@ -3,6 +3,7 @@ package com.joao.simplehelpdesk.services;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.joao.simplehelpdesk.domain.Chamado;
@@ -12,49 +13,49 @@ import com.joao.simplehelpdesk.domain.enums.Perfil;
 import com.joao.simplehelpdesk.domain.enums.Prioridade;
 import com.joao.simplehelpdesk.domain.enums.Status;
 import com.joao.simplehelpdesk.repositories.ChamadoRepository;
-import com.joao.simplehelpdesk.repositories.ClienteRepository;
-import com.joao.simplehelpdesk.repositories.TecnicoRepository;
+import com.joao.simplehelpdesk.repositories.PessoaRepository;	
 
 @Service
 public class DbService {
 
 	@Autowired
-	private TecnicoRepository tecnicoRepository;
-	@Autowired
-	private ClienteRepository clienteRepository;
+	private PessoaRepository pessoaRepository;
 	
 	@Autowired
 	private ChamadoRepository chamadoRepository;
 	
+	@Autowired
+	private BCryptPasswordEncoder encoder;
+	
 
 	public void instanciaDb() {
 
-		Tecnico tec1 = new Tecnico(null, "Joao", "03029092062", "joao@mail.com", "123455");
+		Tecnico tec1 = new Tecnico(null, "Joao", "03029092062", "joao@mail.com", encoder.encode("123455"));
 		tec1.addPerfil(Perfil.ADMIN);
 
-		Tecnico tec2 = new Tecnico(null, "Maria", "66587045030", "maria@mail.com", "123455");
+		Tecnico tec2 = new Tecnico(null, "Maria", "66587045030", "maria@mail.com", encoder.encode("123455"));
 		tec2.addPerfil(Perfil.TECNICO);
 
-		Tecnico tec3 = new Tecnico(null, "Carlos", "84984999098", "carlos@mail.com", "123455");
+		Tecnico tec3 = new Tecnico(null, "Carlos", "84984999098", "carlos@mail.com", encoder.encode("123455"));
 		tec3.addPerfil(Perfil.TECNICO);
 
-		Tecnico tec4 = new Tecnico(null, "Ana", "53866904002", "ana@mail.com", "123455");
+		Tecnico tec4 = new Tecnico(null, "Ana", "53866904002", "ana@mail.com", encoder.encode("123455"));
 		tec4.addPerfil(Perfil.TECNICO);
 
-		Tecnico tec5 = new Tecnico(null, "Lucas", "19595463094", "lucas@mail.com", "123455");
+		Tecnico tec5 = new Tecnico(null, "Lucas", "19595463094", "lucas@mail.com", encoder.encode("123455"));
 		tec5.addPerfil(Perfil.TECNICO);
 		
 
-		Cliente cli1 = new Cliente(null, "Pedro", "51744627053", "cliente1@mail.com", "123455");
-		Cliente cli2 = new Cliente(null, "Mariana", "60716668050", "cliente2@mail.com", "123455");
-		Cliente cli3 = new Cliente(null, "Roberto", "21940448093", "cliente3@mail.com", "123455");
-		Cliente cli4 = new Cliente(null, "Clara", "86774612059", "cliente4@mail.com", "123455");
-		Cliente cli5 = new Cliente(null, "Felipe", "76737341098", "cliente5@mail.com", "123455");
-		Cliente cli6 = new Cliente(null, "Julia", "77071584079", "cliente6@mail.com", "123455");
-		Cliente cli7 = new Cliente(null, "Renato", "29751602084", "cliente7@mail.com", "123455");
-		Cliente cli8 = new Cliente(null, "Sofia", "98586452017", "cliente8@mail.com", "123455");
-		Cliente cli9 = new Cliente(null, "Vinicius", "84156528014", "cliente9@mail.com", "123455");
-		Cliente cli10 = new Cliente(null, "Larissa", "25404273089", "cliente10@mail.com", "123455");
+		Cliente cli1 = new Cliente(null, "Pedro", "51744627053", "cliente1@mail.com", encoder.encode("123455"));
+		Cliente cli2 = new Cliente(null, "Mariana", "60716668050", "cliente2@mail.com", encoder.encode("123455"));
+		Cliente cli3 = new Cliente(null, "Roberto", "21940448093", "cliente3@mail.com", encoder.encode("123455"));
+		Cliente cli4 = new Cliente(null, "Clara", "86774612059", "cliente4@mail.com", encoder.encode("123455"));
+		Cliente cli5 = new Cliente(null, "Felipe", "76737341098", "cliente5@mail.com", encoder.encode("123455"));
+		Cliente cli6 = new Cliente(null, "Julia", "77071584079", "cliente6@mail.com", encoder.encode("123455"));
+		Cliente cli7 = new Cliente(null, "Renato", "29751602084", "cliente7@mail.com", encoder.encode("123455"));
+		Cliente cli8 = new Cliente(null, "Sofia", "98586452017", "cliente8@mail.com", encoder.encode("123455"));
+		Cliente cli9 = new Cliente(null, "Vinicius", "84156528014", "cliente9@mail.com", encoder.encode("123455"));
+		Cliente cli10 = new Cliente(null, "Larissa", "25404273089", "cliente10@mail.com", encoder.encode("123455"));
 
 		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Credenciais", "Primeiro chamado aberto", tec1, cli1);
 		Chamado c2 = new Chamado(null, Prioridade.ALTA, Status.ABERTO, "Conexão de rede", "Problemas de conexão", tec2, cli2);
@@ -67,8 +68,7 @@ public class DbService {
 		Chamado c9 = new Chamado(null, Prioridade.BAIXA, Status.ANDAMENTO, "Configuração", "Configuração de e-mail concluída", tec4, cli9);
 		Chamado c10 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Segurança", "Configuração de firewall", tec5, cli10);
 
-		tecnicoRepository.saveAll(Arrays.asList(tec1, tec2, tec3, tec4, tec5));
-		clienteRepository.saveAll(Arrays.asList(cli1, cli2, cli3, cli4, cli5, cli6, cli7, cli8, cli9, cli10));
+		pessoaRepository.saveAll(Arrays.asList(cli1, cli2, cli3, cli4, cli5, cli6, cli7, cli8, cli9, cli10,tec1, tec2, tec3, tec4, tec5));
 		chamadoRepository.saveAll(Arrays.asList(c1, c2, c3, c4, c5, c6, c7, c8, c9, c10));
 
 	}
---
+++ src/main/java/com/joao/simplehelpdesk/services/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.joao.simplehelpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.joao.simplehelpdesk.domain.Pessoa;
+import com.joao.simplehelpdesk.repositories.PessoaRepository;
+import com.joao.simplehelpdesk.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Optional<Pessoa> user = pessoaRepository.findByEmail(email);
+		
+		if (user.isPresent()) {
+			return new UserSS(user.get().getId(), user.get().getEmail(), user.get().getSenha(), user.get().getPerfis());
+		}
+		throw new UsernameNotFoundException(email);
+	}
+	
+}
